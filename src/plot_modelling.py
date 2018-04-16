c81e715e719c1c672a530b20ba6237770b872ea6
mdekauwe <mdekauwe@gmail.com>
2018-04-16 12:50:09 +1000

    Added model output

--- src/plot_modelling.py
+++ src/plot_modelling.py
@@ -12,6 +12,12 @@ import datetime as dt
 import numpy as np
 from scipy.stats import gaussian_kde
 
+sys.path.append('Coupled_Canopy')
+from farq import FarquharC3
+from solve_coupled_An_gs_leaf_temp_transpiration import CoupledModel
+from utils import vpd_to_rh, get_dewpoint, calc_esat
+import constants as c
+
 __author__  = "Martin De Kauwe"
 __version__ = "1.0 (16.04.2018)"
 __email__   = "mdekauwe@gmail.com"
@@ -37,6 +43,60 @@ def main(fname):
     PARlimit = 600
     df_ct = df_ct[df_ct["PAR"] > PARlimit]
     df_hw = df_hw[df_hw["PAR"] > PARlimit]
+
+
+    # Parameters
+
+    # A stuff
+    Vcmax25 = 34.0
+    Jmax25 = 60.0
+    Rd25 = 0.92
+    Eaj = 21640.
+    Eav = 51780.
+    deltaSj = 633.0
+    deltaSv = 640.0
+    Hdv = 200000.0
+    Hdj = 200000.0
+    Q10 = 1.92
+
+    # Misc stuff
+    leaf_width = 0.01
+    SW_abs = 0.86 # absorptance to short_wave rad [0,1], typically 0.4-0.6
+
+
+    # variables though obviously fixed here.
+    wind = 8.0
+    pressure = 101325.0
+    g0 = 0.003
+    g1 = 2.9
+    Ca = 400.
+    D0 = 1.5 # kpa # Note used
+    gamma = 0.0
+    C = CoupledModel(g0, g1, D0, gamma, Vcmax25, Jmax25, Rd25,
+                     Eaj, Eav,deltaSj, deltaSv, Hdv, Hdj, Q10, leaf_width,
+                     SW_abs, gs_model="medlyn")
+
+
+    Et_ct = []
+    An_ct = []
+    Et_hw = []
+    An_hw = []
+    for i in range(len(df_ct)):
+
+        (An, gsw, et, LE) = C.main(df_ct.Tair_al[i], df_ct.PAR[i], df_ct.VPD[i],
+                                   wind, pressure, Ca)
+        Et_ct.append(et* c.MOL_2_MMOL) # mmol m-2 s-1
+        An_ct.append(An)               # umol m-2 s-1
+
+    Et_hw = []
+    An_hw = []
+    for i in range(len(df_hw)):
+
+        (An, gsw, et, LE) = C.main(df_hw.Tair_al[i], df_hw.PAR[i], df_hw.VPD[i],
+                                   wind, pressure, Ca)
+        Et_hw.append(et * c.MOL_2_MMOL) # mmol m-2 s-1
+        An_hw.append(An)                # umol m-2 s-1
+
 
     width  = 9.0
     height = width / 1.618
@@ -64,6 +124,7 @@ def main(fname):
     xy = np.vstack([x, y])
     z = gaussian_kde(xy)(xy)
     ax1.scatter(x, y,  c=z, s=25, edgecolor='', cmap='Blues', alpha=0.7)
+    ax1.scatter(x, An_ct, color='black', s=5, alpha=0.7)
 
     x = df_hw.TargTempC_Avg
     y = df_hw.Photo
@@ -72,6 +133,7 @@ def main(fname):
     xy = np.vstack([x, y])
     z = gaussian_kde(xy)(xy)
     ax2.scatter(x, y,  c=z, s=25, edgecolor='', cmap='Reds', alpha=0.7)
+    ax2.scatter(x, An_hw, color='black', s=5, alpha=0.7)
 
     x = df_ct.TargTempC_Avg
     y = df_ct.Trans
@@ -80,6 +142,7 @@ def main(fname):
     xy = np.vstack([x, y])
     z = gaussian_kde(xy)(xy)
     ax3.scatter(x, y,  c=z, s=25, edgecolor='', cmap='Blues', alpha=0.7)
+    ax3.scatter(x, Et_ct, color='black', s=5, alpha=0.7)
 
     x = df_hw.TargTempC_Avg
     y = df_hw.Trans
@@ -88,6 +151,7 @@ def main(fname):
     xy = np.vstack([x, y])
     z = gaussian_kde(xy)(xy)
     ax4.scatter(x, y,  c=z, s=25, edgecolor='', cmap='Reds', alpha=0.7)
+    ax4.scatter(x, Et_hw, color='black', s=5, alpha=0.7)
 
     ax1.set_ylim(0, 12)
     ax2.set_ylim(0, 12)
